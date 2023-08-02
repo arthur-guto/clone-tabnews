5526d119d06afcecccbbc08d8505c94f099cb9cf
Arthur Augusto <113945022+arthur-guto@users.noreply.github.com>
2023-08-02 00:01:18 +0000

    clone do repositório antigo do clone-tabnews

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+    return <h1><figure>
+    <blockquote>
+        "Apenas estudar, sem nenhuma experiência prática dos ensinamentos, não é suficiente. 
+           Apenas praticar sem nenhuma base teórica também pode cair em falácias, erros, faltas."
+    </blockquote>
+    <footer>
+        — <cite class="author">Coen Rōshi</cite>
+    </footer>
+</figure></h1>
+}
+
+export default Home;
