32c7bbdad1de3cacfb1f04c96fd464d9f5e25153
Arthur Augusto <113945022+arthur-guto@users.noreply.github.com>
2023-08-02 19:54:22 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 function Home() {
-    return <h1><figure>
-    <blockquote>
-        "Apenas estudar, sem nenhuma experiência prática dos ensinamentos, não é suficiente. 
-           Apenas praticar sem nenhuma base teórica também pode cair em falácias, erros, faltas."
-    </blockquote>
-    <footer>
-        — <cite class="author">Coen Rōshi</cite>
-    </footer>
-</figure></h1>
+  return (
+    <h1>
+      "Apenas estudar, sem nenhuma experiência prática dos ensinamentos, não é
+      suficiente. Apenas praticar sem nenhuma base teórica também pode cair em
+      falácias, erros, faltas."
+    </h1>
+  );
 }
 
 export default Home;
